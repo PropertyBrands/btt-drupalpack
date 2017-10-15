c258b79b4d346455066c78dba512ec90d7ca6385
ethanhinson <ethan@bluetent.com>
2017-10-14 19:41:01 -0600

    Fix production build settings

--- starter/webpack.config.js
+++ starter/webpack.config.js
@@ -97,9 +97,9 @@ const config = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  config.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
+  config.plugins = (config.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
